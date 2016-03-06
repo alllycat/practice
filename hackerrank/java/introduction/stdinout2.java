659b96ae953041db2f8fe68f10b255b2967c05a3
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-06 17:01:52 -0500

    Create stdinout2.java

---
+++ hackerrank/java/introduction/stdinout2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int x=sc.nextInt();
+            double y=sc.nextDouble();
+            String e=sc.nextLine(); // will be empty
+            String s=sc.nextLine(); // new string
+            
+         
+
+            System.out.println("String: "+s);
+            System.out.println("Double: "+y);
+            System.out.println("Int: "+x);
+    }
+}
+

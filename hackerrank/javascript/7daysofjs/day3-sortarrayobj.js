6b634dc35c8d290d9b07228be33207eb12469560
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-12 21:33:25 -0500

    Create day3-sortarrayobj.js

---
+++ hackerrank/javascript/7daysofjs/day3-sortarrayobj.js
@@ -0,0 +1,10 @@
+/*Objective: Practice sorting an array.
+
+Resources - Here are some quick tutorials on sort: 
+JavaScript Array sort() http://www.w3schools.com/jsref/jsref_sort.asp
+Sorting an Array of Objects by Property https://davidwalsh.name/array-sort
+
+Task: An array of objects, librarylibrary, is provided for you in the editor. 
+Complete the code below so that it sorts library's elements alphabetically by book name and then prints library.
+
+Note: There is no input to be read, and there are no sample test cases.*/

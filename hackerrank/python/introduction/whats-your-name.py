3bf0da93c82f84fbb58972d7b18394d39b0ca38f
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-14 03:18:29 -0400

    Create whats-your-name.py

---
+++ hackerrank/python/introduction/whats-your-name.py
@@ -0,0 +1,10 @@
+'''Let's learn the basics of Python! You are given the first and last name of a person. Your task is to read them and print the following:
+
+>>>Hello firstname lastname! You just delved into python.'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+firstName = raw_input()
+lastName = raw_input()
+
+print("Hello " + firstName + " " + lastName + "! You just delved into python.")

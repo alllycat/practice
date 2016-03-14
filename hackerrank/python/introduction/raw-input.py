a45a6a2993798c0262d0eb3526fce88112f47cc4
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-14 02:40:26 -0400

    Create raw-input.py

---
+++ hackerrank/python/introduction/raw-input.py
@@ -0,0 +1,14 @@
+'''Task: Read a string from the console and print it.
+
+Input Format: The first and only line of input contains a string.
+
+Constraints: String length ≤ 500
+
+Output Format: Print the same string back.
+
+Sample Input: How many chickens does it take to cross the road?
+Sample Output: How many chickens does it take to cross the road?'''
+
+''' Enter your code here. Read input from STDIN. Print output to STDOUT '''
+str = raw_input()
+print(str)

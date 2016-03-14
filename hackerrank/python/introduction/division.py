56e2bb661272a110a3bb4dd6d55f0a27bcbba3be
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-14 02:54:43 -0400

    Create division.py

---
+++ hackerrank/python/introduction/division.py
@@ -0,0 +1,19 @@
+''' Task 
+Read two integers and print two lines. The first line should contain integer division, a//b. The second line should contain float division, a/b.
+
+You don't need to perform any rounding or formatting operations.
+
+Input Format 
+The first line contains the first integer, a. The second line contains the second integer, b.
+
+Output Format 
+Print the two lines as described above.'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from __future__ import division # not necessary in python 3, but necessary in python 2
+
+a = int(raw_input())
+b = int(raw_input())
+
+print(a//b)
+print(a/b)

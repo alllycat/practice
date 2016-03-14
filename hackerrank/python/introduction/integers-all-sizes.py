182b676548583510dee7b6c5a2df57a22fdae3f7
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-14 03:08:09 -0400

    Create integers-all-sizes.py

---
+++ hackerrank/python/introduction/integers-all-sizes.py
@@ -0,0 +1,14 @@
+'''Task 
+Read four numbers, a, b, c, and d, and print the result of ab+cdab+cd.
+
+Input Format 
+Integers aa, bb, cc, and dd are given on four separate lines, respectively.'''
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(raw_input())
+b = int(raw_input())
+c = int(raw_input())
+d = int(raw_input())
+
+print(pow(a,b) + pow(c,d))

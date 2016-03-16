ba5948d667a112f46b7b2cad9760bd0d9ad8210f
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-16 00:18:35 -0400

    update lists.py with method 1

--- hackerrank/python/data-types/lists.py
+++ hackerrank/python/data-types/lists.py
@@ -25,17 +25,27 @@ Sample Output
 [1, 5, 9, 10]
 [9, 5, 1]'''
 
+T = int(raw_input().strip())
+
 L = []
-L.insert(0,5)
-L.insert(1,10)
-L.insert(0,6)
-print(L)
-L.remove(6)
-L.append(9)
-L.append(1)
-L.sort()
-print(L)
-L.pop()
-L.reverse()
-print(L)
+for t in range(T):
+    args = raw_input().strip().split(" ")
+    if args[0] == "append":
+        L.append(int(args[1]))
+    elif args[0] == "insert":
+        L.insert(int(args[1]), int(args[2]))
+    elif args[0] == "remove":
+        L.remove(int(args[1]))
+    elif args[0] == "pop":
+        L.pop()
+    elif args[0] == "sort":
+        L.sort()
+    elif args[0] == "reverse":
+        L.reverse()
+    elif args[0] == "print":
+        print L
+        
+#OR
+
+
 

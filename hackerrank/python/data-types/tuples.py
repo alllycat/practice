51b34e8e83ff4dc2f216d16256076a9dd99ad6f7
Alyssia Jovellanos <alyssia.j.13@hotmail.com>
2016-03-16 00:42:00 -0400

    Create tuples.py

---
+++ hackerrank/python/data-types/tuples.py
@@ -0,0 +1,12 @@
+'''Task 
+You are given an integer NN on one line. The next line contains NN space separated integers. Create a tuple of those NN integers. Let's call it TT. 
+Compute hash(T) and print it.
+
+Note: Here, hash() is one of the functions in the __builtins__ module.
+
+Input Format 
+The first line contains NN. The next line contains NN space separated integers.
+
+Output Format 
+Print the computed value.'''
+
